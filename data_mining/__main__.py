2794158a21d2ace6d7c1bcaa0985c7d4dbc6a756
Beir Bear <beir.bear@gmail.com>
2016-04-20 17:20:45 +0200

    for test run on the SNIC.

--- data_mining/__main__.py
+++ data_mining/__main__.py
@@ -6,3 +6,4 @@ SOURCE_FILE = '/Users/beir/Downloads/sandbox/linkage.p'
 if __name__ == '__main__':
     source_object = LocalDataSource.get_distance_object(SOURCE_FILE)
     heat_map = HeatMap(source_object)
+    print heat_map.get_all_labels()
