fd0c235ccef7dba3eaecea7539e43927c3e374d9
Beir Bear <beir.bear@gmail.com>
2016-05-10 21:57:23 +0200

    for test run on the SNIC.

--- data_mining/__main__.py
+++ data_mining/__main__.py
@@ -6,31 +6,38 @@ from .techniques.hierarchical_clustering import HeatMap
 from .data_source import RemoteDataSource
 from .data_preparation import ProcessingMethod
 from .configuration import Setting, Definition
-
+import time
 
 if __name__ == '__main__':
 
     # Read configuration from file
     Setting.read_configuration_from_file()
-    source_object = RemoteDataSource()
 
-    """The data source should be repeated check for update from the data repository."""
+    while True:
+        source_object = RemoteDataSource()
 
-    # Check total records for test connection
-    print ("Total Records", source_object.total_record)
+        """The data source should be repeated check for update from the data repository."""
 
-    # Pre-processing data
-    fit_object = ProcessingMethod.fit_threshold(source_object.get_all_features())
+        # Check total records for test connection
+        print ("Total Records", source_object.total_record)
 
-    # Generate distance matrix
-    heat_map = HeatMap(fit_object)
+        # Pre-processing data
+        fit_object = ProcessingMethod.fit_threshold(source_object.get_all_features())
 
-    # Push linkage matrix back
-    source_object.push_to_data_repo(Definition.RemoteSource.get_string_push_linkage_matrix(),
-                                    fit_object.linkage_matrix)
-    # Push row_index back
-    source_object.push_to_data_repo(Definition.RemoteSource.get_string_push_row_index(),
-                                    source_object.id_link)
-    # Push label_tree back
-    source_object.push_to_data_repo(Definition.RemoteSource.get_string_push_label_tree(),
-                                    heat_map.get_all_labels())
+        # Generate distance matrix
+        heat_map = HeatMap(fit_object)
+
+        # Push linkage matrix back
+        source_object.push_to_data_repo(Definition.RemoteSource.get_string_push_linkage_matrix(),
+                                        fit_object.linkage_matrix)
+        # Push row_index back
+        source_object.push_to_data_repo(Definition.RemoteSource.get_string_push_row_index(),
+                                        source_object.id_link)
+        # Push label_tree back
+        source_object.push_to_data_repo(Definition.RemoteSource.get_string_push_label_tree(),
+                                        heat_map.get_all_labels())
+
+        if source_object.total_record == 1000:
+            break
+
+        time.sleep(60)
